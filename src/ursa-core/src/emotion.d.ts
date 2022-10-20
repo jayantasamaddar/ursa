2c7df4db35ff7271a6dfe33c6bd545a4be45648d
Jayanta Samaddar <jay.s@kollablifestyle.com>
2022-10-20 20:35:21 +0530

    Merge pull request #2 from jayantasamaddar/dev
    Made rollup config changes. Fixed build package

--- src/ursa-core/src/emotion.d.ts
+++ src/ursa-core/src/emotion.d.ts
@@ -3,9 +3,9 @@ import { Color, Font, FontSize, Border } from './types/theme';
 
 declare module '@emotion/react' {
   export interface Theme {
-    color: Color,
-    font: Font,
-    fontSize: FontSize,
-    border: Border
+    color: Color;
+    font: Font;
+    fontSize: FontSize;
+    border: Border;
   }
 }
--- src/ursa-core/src/global.d.ts
+++ src/ursa-core/src/global.d.ts
@@ -1,8 +1,8 @@
 declare module '*.svg.?js' {
-    import React from 'react';
-    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
+  import React from 'react';
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
 }
 
 type SVGRComponent = React.FC<React.SVGProps<SVGSVGElement>>;
